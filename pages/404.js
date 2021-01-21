2a7ff72eafc092d0bed72f9ebf43769c3932bb37
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2021-01-21 18:43:18 -0500

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,10 @@
+import Layout from '../components/layout'
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <Layout>
+            <h1>404 - Page Not Found</h1>
+            <p>Much Like U2, you still haven't found what you're looking for...</p>
+        </Layout>
+    )
 }
